9d5b432586ecf351ce2ab9aa1bff0ba872b6a7a3
Sebastian Rossi <sebastian.rossi@maibornwolff.de>
2025-02-26 17:35:20 +0100

    bla

--- scripts/script.py
+++ scripts/script.py
@@ -4,6 +4,107 @@ import apriltag
 import math
 
 
+class InterestingRect:
+    def __init__(self, p1: np.array, p2: np.array, p3: np.array, p4: np.array):
+        self.points = np.array([
+            p1, p2, p3, p4
+        ])
+    
+    def __get_sorted_points(self):
+        center = np.mean(self.points, axis=0)
+        
+        def angle_from_center(point):
+            return math.atan2(point[1] - center[1], point[0] - center[0])
+        
+        sorted_points = sorted(self.points, key=angle_from_center)
+        return np.array(sorted_points)
+
+    def __get_trapezoid(self, image) -> np.array:
+        # need to sort points, so that the polygon does not overlap with itself
+        pts = self.__get_sorted_points().astype(float32)
+
+        mask = np.zeros_like(image)
+        cv2.fillPoly(mask, [pts], (255, 255, 255))
+        trapezoid = cv2.bitwise_and(image, mask)
+        x, y, w, h = self.__get_bounding_rect()
+
+        cropped = trapezoid[
+            y : y + h, 
+            x : x + w
+            ]
+        return cropped
+
+    def __get_bounding_rect(self):
+        rect = cv2.boundingRect(pts)  # Tuple: x, y, w, h
+        return rect
+    
+    def get_center(self, local=False):
+        x, y, w, h = self.__get_bounding_rect()
+        if local:
+            return np.array([w/2, h/2])
+        return np.array([x + w/2, y + h/2])
+
+    def get_raw_image(self, frame) -> np.array:
+        trapezoid = self.__get_trapezoid(frame)
+        return trapezoid
+
+    def get_warped_image(self, frame, pose, width, height) -> np.array:
+        """
+        returns the perspective-corrected image
+        """
+        trapezoid = self.__get_trapezoid(frame)
+
+        original_points = points.astype(np.float32)
+        x, y, w, h = self.__get_bounding_rect()
+        local_center = self.get_center(local=True)
+
+        target_rect = np.float32([
+            [0, 0],
+            [0, height],
+            [width, height],
+            [width, 0],
+        ])
+
+        M = cv2.getPerspectiveTransform(target_rect, original_points)
+        M_inv = np.linalg.inv(M)
+
+        if trapezoid.shape[0] > 0 and trapezoid.shape[1] > 0:
+            trapezoid = cv2.warpPerspective(image, M_inv, (w, h))
+        return trapezoid
+
+
+    def get_rotated_image(self, frame, pose) -> np.array:
+        """
+        returns the rotation-corrected image
+        """
+        trapezoid = self.__get_trapezoid(frame)
+
+        rotation_part = pose[:2, :2]
+        translation_part = pose[:2, 3]  # atually irrelevant
+        initial_transformation_matrix = np.hstack([rotation_part.T, translation_part.reshape(2, 1)])
+        
+        center = self.get_center(local=True)
+
+        # rotate around local center = move to the upper left, rotate then move back
+        trans_center = np.array([
+            [1, 0, -center[1]],
+            [0, 1, -center[0]]
+        ], dtype=np.float32)
+
+        reverse_trans_center = np.array([
+            [1, 0, center[1]],
+            [0, 1, center[0]]
+        ], dtype=np.float32)
+
+        # Combine the transformations
+        transformation_matrix = reverse_trans_center @ np.vstack([initial_transformation_matrix, [0, 0, 1]]) @ np.vstack([trans_center, [0, 0, 1]])
+        final_transformation_matrix = transformation_matrix[:2, :]
+        if trapezoid.shape[0] > 0 and trapezoid.shape[1] > 0:
+            trapezoid = cv2.warpAffine(trapezoid, transformation_matrix, (w, h))
+        
+        return trapezoid
+
+
 def sort_points_clockwise(points):
     center = np.mean(points, axis=0)
     
@@ -25,7 +126,10 @@ def cut_and_warp_trapezoid(image, points, pose):
     x, y, w, h = rect
     
     center = np.array([w/2, h/2])
-    cropped_trapezoid = trapezoid[y:y+h, x:x+w]
+    cropped_trapezoid = trapezoid[
+        y : y + h, 
+        x : x + w
+        ]
     
     if False:  # revert the rotation only
         rotation_part = pose[:2, :2]
@@ -50,8 +154,9 @@ def cut_and_warp_trapezoid(image, points, pose):
             cropped_trapezoid = cv2.warpAffine(cropped_trapezoid, transformation_matrix, (w, h))
     else: # revert complete pose
         original_points = points.astype(np.float32)
-        w = max(w, h)
-        h = w
+        #w = max(w, h)
+        #h = w
+        h, w = 200, 200
         transformed_points = np.float32([
             [0, 0],
             [0, h],
@@ -128,7 +233,10 @@ def main():
             small = cut_and_warp_trapezoid(frame, image_points, pose)
             if small.shape[0] > 0 and small.shape[1] > 0:
                 cv2.imshow('rect only', small)
-        cv2.imshow('Labeled detection', frame)
+
+            ir = InterestingRect(*image_points)
+
+        cv2.imshow('Labeled detection', ir.get_warped_image())
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break

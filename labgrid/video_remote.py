8e6e1afd34a9e7a4816dcb1d77eedd32398c72ce
Sebastian Rossi <sebastian.rossi@elkiro.de>
2025-03-17 14:09:06 +0100

    restructuring without direct gstreamer to opencv support (since it needs to be compiled from source for that))

--- labgrid/video_remote.py
+++ labgrid/video_remote.py
@@ -3,6 +3,12 @@ import time
 
 from labgrid import Environment
 from labgrid.logging import basicConfig, StepLogger
+import os
+import cv2
+
+# Get the absolute path of the script file
+
+parent_folder = os.path.abspath(os.path.dirname(__file__))
 
 # enable info logging
 basicConfig(level=logging.INFO)
@@ -10,11 +16,37 @@ basicConfig(level=logging.INFO)
 # show labgrid steps on the console
 StepLogger.start()
 
-e = Environment("/home/rossi/repos/april-tag-tracking-stuff/labgrid/import-video.yaml")
+a = cv2.IStreamReader()
+e = Environment(os.path.join(parent_folder, "import-video.yaml"))
 t = e.get_target()
 
 p = t.get_driver("USBVideoDriver")
 print(p.video.path)
-p.get_opencv_cap(caps_hint="mid")
+p.start_stream(caps_hint="mid")
+#cap = cv2.VideoCapture(p, apiPreference=cv2.CAP_GSTREAMER, params=[])
+
+# if gstreamer opencv support
+#gst_pipeline = (
+#    "shmsrc socket-path=/tmp/gst-shm ! "
+#    "video/x-raw,format=BGR,width=1280,height=720,framerate=30/1 ! "
+#    "videoconvert ! appsink sync=false max-buffers=1 drop=true"
+#)
+#print(gst_pipeline)
+#cap = cv2.VideoCapture(gst_pipeline, apiPreference=cv2.CAP_GSTREAMER)
+#if not cap.isOpened():
+#    print("Error: Could not open GStreamer pipeline.")
+#    exit()
+
+while p.is_stream_open():
+    ret, frame = p.read()
+    print(frame.shape)
+
+    if ret:
+        cv2.imshow("video", frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+p.stop_stream()
+cv2.destroyAllWindows()
+
 print(p)
 

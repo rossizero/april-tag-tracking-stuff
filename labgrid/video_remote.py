0461d339844b1f7ea928dcf3043f705d86acc242
Sebastian Rossi <sebastian.rossi@elkiro.de>
2025-03-18 09:08:09 +0100

    video_remote good to go

--- labgrid/video_remote.py
+++ labgrid/video_remote.py
@@ -7,24 +7,18 @@ from labgrid.logging import basicConfig, StepLogger
 import os
 import cv2
 
-# Get the absolute path of the script file
-
 parent_folder = os.path.abspath(os.path.dirname(__file__))
 
-# enable info logging
 basicConfig(level=logging.INFO)
-
-# show labgrid steps on the console
 StepLogger.start()
 
-a = cv2.IStreamReader()
 e = Environment(os.path.join(parent_folder, "import-video.yaml"))
 t = e.get_target()
 
 p = t.get_driver("USBVideoDriver")
 print(p.video.path)
 
-p.start_stream(caps_hint="high")
+p.start_stream(caps_hint="mid")
 
 while p.is_stream_open():
     ret, frame = p.read()
@@ -33,8 +27,7 @@ while p.is_stream_open():
         cv2.imshow("video", frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+
 p.stop_stream()
 cv2.destroyAllWindows()
-
-print(p)
 
